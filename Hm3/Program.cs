17531464c66a764c9dc9ddf817678a50aad6b450
Sardor Bobojonov <sardor.bobojonov@gmail.com>
2022-10-06 01:29:42 +0500

    initial commit

---
+++ Hm3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Не использовать строки для расчета.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 326792 -> 6
+
+System.Console.Write("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+if (value > 99)
+{
+    if (value < 1000)
+    {
+        int result1 = value % 10;
+        System.Console.WriteLine(result1);
+    }
+    else
+    {
+        while (value > 999)
+        {
+            value = value / 10;
+        }
+        int result2 = value % 10;
+        System.Console.WriteLine(result2);
+    }
+}
+else
+{
+    System.Console.WriteLine($"{value} -> третьей цифры нет");
+}
---
+++ Hm4/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Number(string message)
+{
+    System.Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int day = Number("Введите день недели цифрой: ");
+
+if (day == 1)
+{
+    System.Console.WriteLine($"{day} -> нет");
+}
+if (day == 2)
+{
+    System.Console.WriteLine($"{day} -> нет");
+}
+if (day == 3)
+{
+    System.Console.WriteLine($"{day} -> нет");
+}
+if (day == 4)
+{
+    System.Console.WriteLine($"{day} -> нет");
+}
+if (day == 5)
+{
+    System.Console.WriteLine($"{day} -> нет");
+}
+if (day == 6)
+{
+    System.Console.WriteLine($"{day} -> Выходной!");
+}
+if (day == 7)
+{
+    System.Console.WriteLine($"{day} -> Выходной!");
+}
+if (day <= 0 || day > 7)
+{
+    System.Console.WriteLine("В неделе всего 7 дней");
+}
